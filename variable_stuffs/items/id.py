2c298700f7f6065fcd29074ea6b6cad8409bf0fe
GamoupIndustriesEst2019 <72953048+GamoupIndustriesEst2019@users.noreply.github.com>
2021-10-30 11:01:42 -0500

    Create id.py

---
+++ variable_stuffs/items/id.py
@@ -0,0 +1,10 @@
+# Equipment ID
+id_xx = None
+
+# Item IDs
+# Chapter 1
+id_00 = 0x00 # Pocket Knife
+id_01 = 0x01 # Bread Roll
+id_02 = 0x02 # Fresh Milk Carton
+id_03 = 0x03 # Spoiled Milk Carton
+id_04 = 0x04 # Empty Milk Carton
