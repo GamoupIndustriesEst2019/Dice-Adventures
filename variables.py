46eb6df9f1d9790464b652827b5309217b99531b
GamoupIndustriesEst2019 <72953048+GamoupIndustriesEst2019@users.noreply.github.com>
2021-10-30 10:47:49 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Characters
+from variable_stuffs.characters.names import *
+from variable_stuffs.characters.genders import *
+from variable_stuffs.characters.species import *
+
+# Items
+
+
+# Switches
