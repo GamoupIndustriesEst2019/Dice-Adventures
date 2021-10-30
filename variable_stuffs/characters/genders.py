cbf09b29ac1a411b080691390ad85d22369d5ad6
GamoupIndustriesEst2019 <72953048+GamoupIndustriesEst2019@users.noreply.github.com>
2021-10-30 10:51:57 -0500

    Create genders.py

---
+++ variable_stuffs/characters/genders.py
@@ -0,0 +1,4 @@
+# Genders
+male = "Male"     # For Jordan, Bandit, Grimm, Domino, Dudley, Buttons, Booboo, Tristan, and Wick
+female = "Female" # For Sophie, Ava, and Alexis
+nb = "Non-binary" # Unused
