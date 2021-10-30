f2ade505d57e7c655f3eefb3b0b53494ae7d3e39
GamoupIndustriesEst2019 <72953048+GamoupIndustriesEst2019@users.noreply.github.com>
2021-10-30 09:14:35 -0500

    Delete start directory

--- start/global_variables.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, '/Downloads/ZWMC/games/Dice Adventures/other')
-from other import command_help
-pocket_knife_counter = 1
-location = 0
-kitchen_interact = 0
-command_help.command_beginning()
