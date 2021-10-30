17db9759898fead3165b93902e8aedaf3aa85924
GamoupIndustriesEst2019 <72953048+GamoupIndustriesEst2019@users.noreply.github.com>
2021-10-30 10:54:50 -0500

    Create species.py

---
+++ variable_stuffs/characters/species.py
@@ -0,0 +1,6 @@
+# Species
+human = "Human"       # For Jordan, Tristan, Alexis, and Ava
+dog = "Dog"           # For Bandit, Sophie, Domino, Dudley, Buttons, Booboo, Wick
+cat = "Cat"           # Unused
+doghyb = "Dog Hybrid" # For Grimm
+cathyb = "Cat Hybrid" # Unused
